a3bdcc33f47c626b1787d60a87ba9fc47acca067
Miguel Vera <miguel.vera7908@gmail.com>
2020-10-04 18:31:48 -0500

    Getters and Return Values exercise

--- src/App.java
+++ src/App.java
@@ -5,11 +5,27 @@ class Person{
 
 
     void speak(){
-        System.out.println("My name is: " + name + " and I am " + age + " years old. ");
+        System.out.println("My name is: " + name);
     }
 
     void sayHello(){
         System.out.println("Hello there!");
+    }
+
+    int calculateYearsToRetirement(){
+        int yearsLeft = 65 - age;
+
+        return yearsLeft;
+    }
+
+    //Getters
+    int getAge(){
+
+        return age;
+    }
+
+    String getName(){
+        return name;
     }
 }
 
@@ -29,16 +45,19 @@ public class App {
         person1.sayHello();
         person1.name =  "Miguel";
         person1.age = 30;
-        person1.speak();
+//        person1.speak();
 
 
-        Person person2 = new Person();
-        person2.name = "Anderson Cardoso";
-        person2.age = 27;
-        person2.speak();
+        int years = person1.calculateYearsToRetirement();
+
+        System.out.println("Years until retirement " + years);
+
+        int age = person1.getAge();
+        String name = person1.getName();
 
 
-
+        System.out.println("Name is: " + name);
+        System.out.println("Age is: " + age);
 
     }
 }

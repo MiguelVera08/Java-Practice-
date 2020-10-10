45637bc26887b50754cf62d3236640cad332d797
Miguel Vera <miguel.vera7908@gmail.com>
2020-10-09 21:41:20 -0500

    Encapsulation exercise

---
+++ src/Encapsulation/App.java
@@ -0,0 +1,32 @@
+package Encapsulation;
+
+class Plant {
+
+    public static final int ID = 7;
+    private String name;
+
+    public String getData(){
+
+        return "some stuff" + calculateGrowthForecast();
+    }
+
+    private int calculateGrowthForecast(){
+        return 9;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
+public class App {
+
+    public static void main(String[] args){
+
+    }
+}
